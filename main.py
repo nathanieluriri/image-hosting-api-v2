b2bd96573cb23e0106073d8349fb3a2054d2c90a
nathanieluriri <uririnathaniel@gmail.com>
2025-06-06 20:31:07 +0100

    new image upload api

--- main.py
+++ main.py
@@ -20,34 +20,6 @@ app.add_middleware(
 class ImageData(BaseModel):
     base64_image: str
 
-# @app.post("/upload")
-# async def upload_image(image_data: ImageData):
-#     try:
-#         from db import initialize_counter, insert_url_document      
-#         # Prepare the request to Freeimage.host
-#         response = requests.post(
-#             "https://freeimage.host/api/1/upload",
-#             data={
-#                 "key": "6d207e02198a847aa98d0a2a901485a5",  # Replace with your Freeimage.host API key
-#                 "source": image_data.base64_image,
-#                 "format": "json",
-#             },
-#         )
-
-#         # Check if the response was successful
-#         if response.status_code == 200:
-#             initialize_counter()
-#             id=insert_url_document(response.json()['image']['url'])
-#             return id
-#         else:
-#             raise HTTPException(status_code=response.status_code, detail=response.text)
-    
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-
-
-# Instead of requests
 
 @app.post("/upload")
 async def upload_image(image_data: ImageData):
@@ -68,14 +40,12 @@ async def upload_image(image_data: ImageData):
         if response.status_code == 200:
             initialize_counter()
             id = insert_url_document(response.json()['image']['url'])
-            return id
+            return {"url":response.json()['image']['url']}
         else:
             raise HTTPException(status_code=response.status_code, detail=response.text)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-# To run the FastAPI app, use the command: uvicorn filename:app --reload
 
 @app.get("/")
 async def root():
